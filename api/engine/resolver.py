cff3a6be90c5afddc7c96da3a055fd5e5fb18408
ujjwal-basnet <ujjwalvictus15@gmail.com>
2026-04-29 09:30:22 +0545

    Add api/engine/resolver.py

---
+++ api/engine/resolver.py
@@ -0,0 +1,5 @@
+"""Follow-up query resolution helpers."""
+
+from api.engine import _needs_resolver, _sanitize_text, detect_language
+
+__all__ = ["_needs_resolver", "_sanitize_text", "detect_language"]
